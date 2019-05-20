a40b4ed518b2594353ab44f32e6e104ad314216c
Nelson Diaz <abnerdc86@gmail.com>
2019-05-20 18:21:03 -0400

    reverse int and maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let i of str) {
+    charMap[i] = charMap[i] + 1 || 1;
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
