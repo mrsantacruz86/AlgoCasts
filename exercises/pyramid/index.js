739156b97d367ee9757b3f2721c982a478f5277f
Nelson Diaz <abnerdc86@gmail.com>
2019-05-23 18:22:47 -0400

    pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,43 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// Recursive solution
+function pyramid(n, row = 0, level = "") {
+  if (row === n) {
+    return;
+  }
 
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = "#";
+  } else {
+    add = " ";
+  }
+  // console.log(level)
+  pyramid(n, row, level + add)
+}
+
+pyramid(5);
 module.exports = pyramid;
+
+
+// Iterative solution
+// function pyramid(n) {
+//   for (let row = 1; row <= n; row++) {
+//     let stair = "";
+//     for (let col = 1; col <= n * 2 - 1; col++) {
+//       if (col > n - row && col < n + row) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+//     console.log(stair)
+//   }
+// }
