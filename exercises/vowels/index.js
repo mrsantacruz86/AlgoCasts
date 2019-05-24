e8ad53017d6e2b44500cc4891d786c297b35db46
Nelson Diaz <abnerdc86@gmail.com>
2019-05-24 00:52:16 -0400

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,22 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  console.log(str,matches);
+  return matches ? matches.length : 0;
+}
+
+
+// function vowels(str) {
+//   let count =0;
+//   let vowelList = ["a", "e", "i", "o", "u"];
+//   for (let char of str.toLowerCase()) {
+//     if(vowelList.includes(char)){
+//       count++;
+//     }
+//   }
+//   return count;
+// }
 
 module.exports = vowels;
