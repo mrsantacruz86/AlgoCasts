307d6b377edaab87a3e48c86446e1a046d9cef25
Nelson Diaz <abnerdc86@gmail.com>
2019-06-02 02:43:13 -0400

    merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,67 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < (arr.length - i - 1); j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
 }
 
 function selectionSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    let iMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[iMin]) iMin = j;
+    }
+    if (i !== iMin) {
+      let temp = arr[iMin];
+      arr[iMin] = arr[i];
+      arr[i] = temp;
+    }
+  }
+  return arr;
 }
-
+/**
+ * 
+ * @param {array} arr 
+ */
 function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+
+  const center = Math.floor(arr.length / 2);
+  // slice method cuts from 0 to center but not including center.
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left),  mergeSort(right));
 
 }
 
+/**
+ * 
+ * @param {array} left 
+ * @param {array} right 
+ */
 function merge(left, right) {
+  const results =[];
 
+  while(left.length && right.length){
+    if(left[0] < right[0]){
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
